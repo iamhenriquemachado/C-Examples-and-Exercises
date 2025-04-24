2ce9b1fcbda1a458afd7a9c8681e369738f41969
Henrique Machado <heyhenriquecastro@gmail.com>
2025-04-24 10:53:26 -0300

    OOP concepts practicing in C#
    Constructors, this keyword, method overloading, encapsulation

---
+++ OOP_Concepts_CSharp/BankAccount.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoProgramacao {
+    internal class BankAccount {
+        public int Id { get; set; }
+        public string AccountNumber { get; set; }
+        public string AccountHolder { get; set; }
+        public decimal Balance { get; private set; }
+
+        public string GetBalance() {
+            return $"{AccountHolder} has a balance of {Balance:C}";
+        }
+
+        public string Deposit(decimal amount) {
+            if (amount >= 0) {
+                Balance += amount;
+            }
+            return $"{AccountHolder} deposited {amount:C}. New balance: {Balance:C}";
+        }
+
+        public string Withdraw(decimal amount) {
+            if (amount > Balance) {
+                return $"{AccountHolder} cannot withdraw {amount:C}. Insufficient funds.";
+            } else {
+                Balance -= amount;
+                return $"{AccountHolder} withdrew {amount:C}. New balance: {Balance:C}";
+            }
+        }
+      }
+    }
---
+++ OOP_Concepts_CSharp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using CursoProgramacao;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+
+            BankAccount bank = new BankAccount();
+
+            Console.WriteLine("Insert account number: ");
+            bank.AccountNumber = Console.ReadLine();
+
+            Console.WriteLine("Insert the account holder: ");
+            bank.AccountHolder = Console.ReadLine();
+
+            Console.WriteLine("Insert the initial deposit: ");
+            decimal initialDeposit;
+            while (!decimal.TryParse(Console.ReadLine(), out initialDeposit)) {
+                Console.WriteLine("Invalid input. Please enter a valid decimal value for the initial deposit:");
+            }
+            bank.Deposit(initialDeposit);
+
+            Console.WriteLine("Insert the initial deposit: ");
+            decimal withdrawAmmount;
+            while (!decimal.TryParse(Console.ReadLine(), out withdrawAmmount) || withdrawAmmount < 0) {
+                Console.WriteLine("Invalid input. Please enter a valid non-negative value:");
+            }
+
+            bank.Withdraw(withdrawAmmount);
+
+            Console.WriteLine($"Account created successfully! Balance: {bank.GetBalance()}");
+        }
+    }
+}
