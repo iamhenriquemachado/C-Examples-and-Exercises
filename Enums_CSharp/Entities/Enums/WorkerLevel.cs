de30fc7639638bb01f201aa65271143c8e3cfb49
Henrique Machado <heyhenriquecastro@gmail.com>
2025-05-07 11:03:56 -0300

    Enum practice in C#

---
+++ Enums_CSharp/Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoProgramacao.Entities.Enums
+{
+    public enum WorkerLevel : int
+    {
+        Junior = 1, 
+        Mid_Level = 2, 
+        Senior = 3,
+    }
+}
