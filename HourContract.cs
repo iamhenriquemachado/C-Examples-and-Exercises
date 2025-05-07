befc1da7b3901c011010d67efe1931e63a27b3ee
Henrique Machado <heyhenriquecastro@gmail.com>
2025-05-07 11:02:19 -0300

    Enum practice in C#

---
+++ HourContract.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoProgramacao.Entities
+{
+    internal class HourContract
+    {
+        public DateTime date { get; set; }
+        public double ValuePerHour { get; set; }
+        public int Hours { get; set; }
+
+        public override string ToString()
+        {
+            return $"{date.ToString("yyyy-MM-dd HH:mm:ss")} - ValuePerHour: {ValuePerHour}, Hours: {Hours}";
+        }
+    }
+}
