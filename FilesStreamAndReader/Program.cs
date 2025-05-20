0c5d98829d1b5d02a120625f980c402a83bad461
Henrique Machado <heyhenriquecastro@gmail.com>
2025-05-20 20:03:07 -0300

    FileStream, Reader and System.IO practicing

---
+++ FilesStreamAndReader/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System;
+
+namespace CursoProgramacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\temp\tarefas.txt";
+
+
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(@"C:\temp");
+            }
+
+            using (StreamWriter streamWriter = new StreamWriter(path))
+            {
+                streamWriter.WriteLine("2. Estudar programação");
+                streamWriter.WriteLine("3. Ler livros"); 
+            }
+            using (StreamReader streamReader = new StreamReader(path))
+            {
+                Console.WriteLine(streamReader.ReadToEnd());
+            }
+        }
+    }
+}
