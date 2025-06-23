7d74e04342b8c5f5024d6a84ca907f41ed2c25d5
Henrique Machado <heyhenriquecastro@gmail.com>
2025-06-23 06:23:48 -0300

    Even numbers using list and for each loop

---
+++ CSharp_OOP/Week1/EvenNumbers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBasics.CSharp_OOP.Week1.Even_Numbers
+{
+    public class EvenNumbers
+    {
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
+
+        public List<int> ReturnNumbers(int firstNumber, int secondNumber)
+        {
+            this.FirstNumber = firstNumber;
+            this.SecondNumber = secondNumber;
+
+            List<int> evenNumbers = new List<int>();
+            int count = secondNumber + firstNumber + 1;
+            int[] allNumbers = Enumerable.Range(firstNumber, count).ToArray();
+            
+            foreach(int number in allNumbers)
+            {
+                if(number % 2 == 0)
+                {
+                    evenNumbers.Add(number);
+                }
+            }
+            return evenNumbers;
+        }
+
+    }
+}
