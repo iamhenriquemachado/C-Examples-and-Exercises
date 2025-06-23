f02319bb82d7056f79265a16637e883e983ee6f2
Henrique Machado <heyhenriquecastro@gmail.com>
2025-06-23 06:03:06 -0300

    Practicing switch structure with simple calculator

---
+++ CSharp_OOP/Week1/Calculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBasics.CSharp_OOP.Week1.Calculator
+{
+    public class Calculator
+    {
+        public int FirstValue { get; set; }
+        public int SecondValue { get; set; }
+        public string Operation { get; set; }
+        public int result; 
+
+        public double CalculateResult(string operation, int firstValue, int secondValue)
+        {
+            Operation = operation; 
+            FirstValue = firstValue;
+            SecondValue = secondValue;
+            
+            switch(operation)
+            {
+                case "+":
+                    result = firstValue + secondValue;
+                    break;
+                case "-":
+                    result = firstValue - secondValue;
+                    break;
+                case "/":
+                    result = firstValue / secondValue;
+                    break;
+                case "*":
+                    result = firstValue * secondValue;
+                    break;
+            }
+            return result; 
+            
+
+        }
+    }
+}
