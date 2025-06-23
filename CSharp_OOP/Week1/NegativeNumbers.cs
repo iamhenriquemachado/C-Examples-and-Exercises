e9d015bae7d5e721323309b54e1350de68a1dc7e
Henrique Machado <heyhenriquecastro@gmail.com>
2025-06-23 06:35:11 -0300

    Using while loops

---
+++ CSharp_OOP/Week1/NegativeNumbers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBasics.CSharp_OOP.Week1.Non_Negative_Numbers
+{
+    public class NegativeNumbers
+    {
+        public int Number { get; set; }
+        public int result;
+
+
+        public int Result(int number)
+        {
+            this.Number = number;
+            int sum = 0;
+
+            while (number > 0)
+            {
+                number = int.Parse(Console.ReadLine());
+                if (number > 0)
+                {
+                    sum += number; 
+                }               
+            }
+            return sum;
+        }
+    }
+}
