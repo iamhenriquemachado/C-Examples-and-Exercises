556d7ebec78f513e462dca6fd5d65dccadb634d3
Henrique Machado <heyhenriquecastro@gmail.com>
2025-06-23 05:49:46 -0300

    Practicing OOP Concepts

---
+++ CSharp_OOP/Week1/Imc_Calculator/Imc.cs
@@ -0,0 +1,24 @@
+﻿namespace CSharpBasics.CSharp_POO.Week1.Imc_Calculator
+{
+    public class Imc
+    {
+        public double Weight { get; set; }
+        public double Height { get; set; }
+
+        private double result;
+
+        public double CalculateImc(double weight, double height)
+        {
+            Weight = weight;
+            Height = height;
+
+            this.result = weight / Math.Pow(height, 2); 
+            return result;
+        }
+
+        public override string ToString()
+        {
+            return "Your IMC is: " + result.ToString("F2");
+        }
+    }
+}
