c9baee43682ab324e934165e7a90b5d1493da9d9
Henrique Machado <heyhenriquecastro@gmail.com>
2025-06-24 05:51:25 -0300

    Do-while practicing with interactive menus

---
+++ CSharp_OOP/Week1/InteractiveMenuLoop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int option = 0;
+        int counter = 0;
+
+        do
+        {
+            Console.WriteLine(counter);
+            counter += 1;
+
+            Console.WriteLine("Do you want to continue?");
+            int menu = int.Parse(Console.ReadLine()); 
+            option = menu;
+
+        } while (option != 1);
+
+    }
+}
