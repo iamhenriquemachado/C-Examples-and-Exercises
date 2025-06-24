0f0e5f17a2fb2d935d12660f19bfb8490e66fe39
Henrique Machado <heyhenriquecastro@gmail.com>
2025-06-24 06:13:35 -0300

    Classes with operations

---
+++ CSharp_OOP/Week1/BankAccount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+ 
+    public class BankAccount
+    {
+        public string Holder { get; set; }
+        public double Balance { get; private set; }
+
+        public void Deposit(double deposit)
+        {
+            Balance += deposit;
+        }
+
+        public void Withdrawal(double withdrawal)
+        {
+            Balance -= withdrawal;
+        }
+
+        public double CheckBalance()
+        {
+            return Balance; 
+        }
+    }
+
+}
