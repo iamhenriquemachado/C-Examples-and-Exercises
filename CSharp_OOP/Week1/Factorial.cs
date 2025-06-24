1bcd5ebe6910b3d8114f91a471e4ab7234dc9b0e
Henrique Machado <heyhenriquecastro@gmail.com>
2025-06-24 05:40:18 -0300

    Factorial using recursion

---
+++ CSharp_OOP/Week1/Factorial.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Factorial
+    {
+        public int FactorialNumber { get; set; }
+
+
+        public int FactorialCalculus(int f)
+        {
+            if(f == 0)
+            {
+                return 1;
+            } return f * FactorialCalculus(f - 1);
+
+        }
+    }
+}
